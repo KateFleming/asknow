684f42cdcbeec0305abb9d69b2393b9308cd71f8
Nick Mealey <mealey@partstash.com>
2014-11-21 00:17:23 -0800

    Fixed sticky menu button.

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -3,13 +3,15 @@ $(document).ready(function(){
   var openButton = $('.open-menu');
   openButton.click(function(){
     menu.css({
-      right: 0
+      right: 0,
+      position: "fixed"
     });
     $(this).hide();
   });
   $('.close-menu').click(function(){
     menu.css({
-      right: "-300px"
+      right: "-300px",
+      position: "absolute"
     });
     openButton.show();
   });
