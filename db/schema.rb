b36b0dac011b7bb70f7deede570668a61f0a0f4f
Nick Mealey <mealey@partstash.com>
2014-11-26 15:40:50 -0800

    Added Tag model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141125175238) do
+ActiveRecord::Schema.define(version: 20141126233706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,12 @@ ActiveRecord::Schema.define(version: 20141125175238) do
     t.datetime "updated_at"
     t.integer  "account_id"
     t.integer  "rating"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "votes", force: true do |t|
